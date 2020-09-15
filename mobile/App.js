53cdabc74a903ff99dac2dc487c419d62c9d05be
Rioja <lulyrioja@gmail.com>
2020-09-14 23:02:03 -0300

    :alien: Adding mobile to the project

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Salve quebrada</Text>
+    </View>
+  );
+}
